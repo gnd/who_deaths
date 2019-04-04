f54127110a9786e74d003999d179817e84e0e9eb
gnd <gnd@itchybit.org>
2019-04-04 18:36:29 +0300

    whole eu cancer mortality breakdown by age

--- get_eu_cancer_ages.sh
+++ get_eu_cancer_ages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# this fills the .js files for the cancer_ages.html files
+# this fills the .js files for the eu_cancer_ages.html files
 #########################################################
 eu_names=(AT BE BG CR CY CZ DN EE FI FR DE GR HU IE IR IT LV LT LU MK MT MD ME NL PL PT RO RS SK SI ES SE CH UK)
 eu_codes=(4010 4020 4030 4038 3080 4045 4050 4055 4070 4080 4085 4140 4150 4160 4170 4180 4186 4188 4190 4195 4200 4260 4207 4210 4230 4240 4270 4273 4274 4276 4280 4290 4300 4308)
